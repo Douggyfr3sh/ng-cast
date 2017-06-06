190586802eca322de034e5150671573ba24d9630
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-06 15:25:44 -0400

    Initialized app with live data and createad a YouTube API helper.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,16 @@
 angular.module('video-player')
   .component('app', {
     templateUrl: 'src/templates/app.html',
-    controller: function() {
+    controller: ['youTube', function(youTube) {
       //$scope.videos = window.exampleVideoData;
       //console.log($scope);
+      console.log(youTube);
+      this.youTubeCallback = function(result) {
+        console.log(result);
+        this.videos = result.data.items;
+        this.currentVideo = result.data.items[0];
+      };
+      youTube.searchYoutube('test query', this.youTubeCallback.bind(this));
       this.onClick = ($event) => {
         console.log('app.onClick event =',$event);
         if ($event.srcElement.className === 'video-list-entry-title ng-binding') {
@@ -19,7 +26,7 @@ angular.module('video-player')
       this.videos = window.exampleVideoData;
       this.currentVideo = window.exampleVideoData[0];
       //return $scope;
-    }
+    }]
   });
 
 
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,24 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http){
+  this.searchYoutube = function(queryString, callback) {
+    console.log(window.YOUTUBE_API_KEY);
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part: 'snippet',
+        key: window.YOUTUBE_API_KEY,
+        q: queryString,
+        maxResults: 5,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+    })
+      .then(function successfulCallback(response) {
+        console.log(response);
+        callback(response);
+      }, function errorCallback(response) {
+        console.log('there was an error with the api request: ',response);
+      });
+  };
 });
