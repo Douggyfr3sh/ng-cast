a2d775a6ee5153cd25c82ccae86d8fb957570d2c
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-06 14:36:01 -0400

    Tied together Video Player list and Video Player using click handler in app.js.

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,17 @@ angular.module('video-player')
       //$scope.videos = window.exampleVideoData;
       //console.log($scope);
       this.onClick = ($event) => {
-        console.log('clicked from $scope fxn',$event);
+        console.log('app.onClick event =',$event);
+        if ($event.srcElement.className === 'video-list-entry-title ng-binding') {
+          this.currentVideo = this.videos.filter(function(cur) {
+            console.log('inner text on clk is:', cur.snippet.title, cur.id.videoId);
+            console.log(cur.snippet.title,$event.srcElement.innerText,cur.snippet.title === $event.srcElement.innerText);
+            return cur.snippet.title === $event.srcElement.innerText ? true : false;
+          })[0];
+          console.log(this.currentVideo);
+        }
       };
+      this.textBinding = 'oh hi';
       this.videos = window.exampleVideoData;
       this.currentVideo = window.exampleVideoData[0];
       //return $scope;
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -2,7 +2,8 @@ angular.module('video-player')
 .component('videoListEntry', {
   templateUrl: 'src/templates/videoListEntry.html',
   bindings: {
-    video: '<'
+    video: '<',
+    clickerFxn: '<'
   },
   controller: function($scope, $element, $attrs) {
 
